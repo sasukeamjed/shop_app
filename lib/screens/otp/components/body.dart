0b8e68522ede349693fca9786bd156e15cf46591
amjed <sasukeamjed@gmail.com>
2021-10-24 18:13:02 +0400

    text form filed error

--- lib/screens/otp/components/body.dart
+++ lib/screens/otp/components/body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shop_app/constants.dart';
+import 'package:shop_app/size_config.dart';
 
 class Body extends StatelessWidget {
   const Body({Key? key}) : super(key: key);
@@ -8,15 +9,63 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       width: double.infinity,
-      child: Column(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+        child: Column(
+          children: [
+            Text(
+              "OTP Verification",
+              style: headingStyle,
+            ),
+            Text("We sent your code to +968 95868***"),
+            buildTimer(),
+            OTPForm(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Row buildTimer() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text("This code will expire in "),
+        TweenAnimationBuilder(
+          tween: Tween<double>(begin: 30.0, end: 0),
+          duration: Duration(seconds: 30),
+          builder: (context, double value, child) => Text(
+            "00:${value.toInt()}",
+            style: TextStyle(color: kPrimaryColor),
+          ),
+          onEnd: () {},
+        )
+      ],
+    );
+  }
+}
+
+class OTPForm extends StatefulWidget {
+  const OTPForm({Key? key}) : super(key: key);
+
+  @override
+  _OTPFormState createState() => _OTPFormState();
+}
+
+class _OTPFormState extends State<OTPForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Row(
         children: [
-          Text("OTP Verification", style: headingStyle,),
-          Text("We sent your code to +968 95868***"),
-          Row(
-            children: [
-              Text("This code will expire in "),
-              TweenAnimationBuilder(tween: Tween(begin: 30.0, end: 0), duration: Duration(seconds: 30), builder: (context, value, child)=> Text("${value!.toInt()}"))
-            ],
+          SizedBox(
+            width: getProportionateScreenWidth(60),
+            child: TextFormField(
+              style: TextStyle(
+                fontSize: 24,
+                decoration: InputDecoration(),
+              ),
+            ),
           ),
         ],
       ),
